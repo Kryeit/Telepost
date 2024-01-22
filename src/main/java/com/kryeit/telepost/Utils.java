6f2269611e598f8553f98504c2c113eaf46a0a7d
muriplz <46825658+muriplz@users.noreply.github.com>
2024-01-22 17:50:07 +0100

    /postlist now shows names of offlines owners

--- src/main/java/com/kryeit/telepost/Utils.java
+++ src/main/java/com/kryeit/telepost/Utils.java
@@ -7,6 +7,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.chunk.ChunkManager;
 import net.minecraft.world.chunk.ChunkStatus;
 
+import java.util.UUID;
+
 import static com.kryeit.telepost.post.Post.WORLD;
 
 public class Utils {
@@ -29,7 +31,12 @@ public class Utils {
     }
 
     public static String getNamedPostOwner(NamedPost post) {
-        return Offlines.getNameByUUID(Telepost.getInstance().playerNamedPosts.getElement(post.id()));
+        UUID id = Telepost.getInstance().playerNamedPosts.getElement(post.id());
+        ServerPlayerEntity player = MinecraftServerSupplier.getServer().getPlayerManager().getPlayer(id);
+        if (player != null)
+            return player.getName().getString();
+
+        return Offlines.getNameByUUID(id);
     }
 
     public static void executeCommandAsServer(String command) {
