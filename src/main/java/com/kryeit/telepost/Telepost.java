92f211a425e2d1bf873f19c969e9dffa92175465
muriplz <46825658+muriplz@users.noreply.github.com>
2025-10-02 03:30:46 +0200

    First commit towards new system on Neoforge

--- src/main/java/com/kryeit/telepost/Telepost.java
+++ src/main/java/com/kryeit/telepost/Telepost.java
@@ -1,142 +1,71 @@
 package com.kryeit.telepost;
 
-import com.kryeit.telepost.autonaming.MonthlyCheckRunnable;
-import com.kryeit.telepost.commands.*;
-import com.kryeit.telepost.commands.cooldown.CooldownStorage;
-import com.kryeit.telepost.compat.BlueMapImpl;
-import com.kryeit.telepost.compat.CompatAddon;
+import com.kryeit.telepost.commands.PostCommands;
 import com.kryeit.telepost.config.ConfigReader;
-import com.kryeit.telepost.listeners.ServerTick;
-import com.kryeit.telepost.post.StructureHandler;
-import com.kryeit.telepost.storage.IDatabase;
-import com.kryeit.telepost.storage.LevelDBImpl;
-import com.kryeit.telepost.storage.NamedPostStorage;
-import de.bluecolored.bluemap.api.BlueMapAPI;
-import de.bluecolored.bluemap.api.BlueMapMap;
-import net.fabricmc.api.DedicatedServerModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import com.kryeit.telepost.storage.DatabaseUtils;
+import com.mojang.logging.LogUtils;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Timer;
-import java.util.UUID;
 
-import static com.kryeit.telepost.compat.BlueMapImpl.markerSet;
-import static com.kryeit.telepost.post.Post.WORLD;
+@Mod(Telepost.MODID)
+public class Telepost {
+    public static final String MODID = "telepost";
 
-public class Telepost implements DedicatedServerModInitializer {
+    public static LuckPerms luckPerms = LuckPermsProvider.get();
 
-    private static final Timer MONTHLY_TIMER = new Timer(true);
+    public Telepost() {
+        DatabaseUtils.createTables();
 
-    public static Telepost instance;
-    public static final String ID = "telepost";
-    public static final String NAME = "Telepost";
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
-    public IDatabase database;
-    public static NamedPostStorage playerNamedPosts;
-    public static Map<UUID, UUID> invites = new HashMap<>();
-
-    public static CooldownStorage randomPostCooldown;
-    public static boolean postBuilding = false;
-
-    @Override
-    public void onInitializeServer() {
         try {
-            LOGGER.info("Reading config file...");
-            ConfigReader.readFile(Path.of("config/" + ID));
+            ConfigReader.readFile(Path.of("config/" + MODID));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        initializeDatabases();
-        instance = this;
-        registerCommands();
-        registerDisableEvent();
-        registerEvents();
-        registerMonthlyCheck();
-
-        // Comment this out in dev environment
-        StructureHandler.createStructures();
+        NeoForge.EVENT_BUS.register(this);
     }
 
-    public void registerMonthlyCheck() {
-        if (!ConfigReader.AUTONAMING) return;
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
 
-        long interval = Duration.ofHours(1).toMillis();
-        MONTHLY_TIMER.schedule(new MonthlyCheckRunnable(), interval, interval);
     }
 
-    public void registerCommands() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicatedServer, commandFunction) -> {
-            NearestPost.register(dispatcher);
-            SetHome.register(dispatcher);
-            Home.register(dispatcher);
-            NamePost.register(dispatcher);
-            UnnamePost.register(dispatcher);
-            Invite.register(dispatcher);
-            Visit.register(dispatcher);
-            BuildPosts.register(dispatcher);
-            PostList.register(dispatcher);
-            ForceVisit.register(dispatcher);
-
-            // TODO: It's broken, fix it
-            //RandomPost.register(dispatcher);
-
-            if (CompatAddon.GRIEF_DEFENDER.isLoaded()) {
-                DeletePostClaim.register(dispatcher);
-            }
-            
-            //CommandDumpDB.register(dispatcher);
-        });
-    }
-
-    public void registerEvents() {
-        ServerTickEvents.END_SERVER_TICK.register(new ServerTick());
-
-        if (CompatAddon.BLUEMAP.isLoaded()) {
-            BlueMapAPI.onEnable(api ->
-                    api.getWorld(WORLD).ifPresent(blueWorld -> {
-                        BlueMapImpl.loadMarkerSet();
-                        for (BlueMapMap map : blueWorld.getMaps()) {
-                            map.getMarkerSets().put("posts", markerSet);
-                        }
-                    })
-            );
-        }
-    }
-
-    public void initializeDatabases() {
-        // Database of all posts and homeposts, with their locations and such
-        database = new LevelDBImpl();
-
-        // For /randompost cooldown
-        try {
-            randomPostCooldown = new CooldownStorage("world/" + ID + "/randompostcooldown");
-            playerNamedPosts = new NamedPostStorage("world/" + ID, "playerPosts.properties");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void registerDisableEvent() {
-        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-            database.stop();
-        });
-    }
-
-    public static Telepost getInstance() {
-        return instance;
-    }
-
-    public static IDatabase getDB() {
-        return getInstance().database;
+    @SubscribeEvent
+    public void onCommandRegistration(RegisterCommandsEvent event) {
+        PostCommands.register(event.getDispatcher());
     }
 }
--- src/main/java/com/kryeit/telepost/config/ConfigReader.java
+++ src/main/java/com/kryeit/telepost/config/ConfigReader.java
@@ -1,53 +1,43 @@
 package com.kryeit.telepost.config;
 
-import com.kryeit.telepost.utils.JSONObject;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ConfigReader {
 
-    public static int GAP;
-    public static int WIDTH;
-    public static int WORLDBORDER;
-    public static boolean AUTONAMING;
-    public static List<String> POST_NAMES = new ArrayList<>();
+    public static int POST_GAP;
+    public static int POST_RADIUS;
 
-    private ConfigReader() {
-
-    }
+    private ConfigReader() {}
 
     public static void readFile(Path path) throws IOException {
         String config = readOrCopyFile(path.resolve("config.json"), "/config.json");
-        JSONObject configObject = new JSONObject(config);
-        WIDTH = Integer.parseInt(configObject.getString("post-width"));
-        GAP = Integer.parseInt(configObject.getString("post-gap"));
-        WORLDBORDER = Integer.parseInt(configObject.getString("worldborder"));
-        AUTONAMING = configObject.getBoolean("auto-naming");
 
-        if(configObject.has("next-post-names")) {
-            var postNamesArray = configObject.getArray("next-post-names");
-            for (int i = 0; i < postNamesArray.size(); i++) {
-                POST_NAMES.add(postNamesArray.getString(i));
-            }
-        }
+        JsonObject configObject = JsonParser.parseString(config).getAsJsonObject();
+
+        POST_GAP = configObject.get("post_gap").getAsInt();
+        POST_RADIUS = configObject.get("post_radius").getAsInt();
     }
 
     public static String readOrCopyFile(Path path, String exampleFile) throws IOException {
         File file = path.toFile();
         if (!file.exists()) {
             InputStream stream = ConfigReader.class.getResourceAsStream(exampleFile);
-            if (stream == null) throw new NullPointerException("Cannot load example file");
+            if (stream == null) {
+                throw new NullPointerException("Cannot load example file");
+            }
 
             //noinspection ResultOfMethodCallIgnored
             file.getParentFile().mkdirs();
             Files.copy(stream, path);
         }
+
         return Files.readString(path);
     }
 }
